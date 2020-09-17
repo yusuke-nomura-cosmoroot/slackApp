2999438b27d6f365b67f8f0307da8c0aef785c04
yusuke-nomura-cosmoroot <y_nomura@cosmoroot.co.jp>
2020-09-17 10:21:50 +0900

    設定ファイルから引数を読み込むpythonを作成。 さらに実施日を保存し判定するロジックを追加。

--- ShuttaikinApp.py
+++ ShuttaikinApp.py
@@ -4,7 +4,7 @@ import sys
 import requests
 import json
 
-WEB_HOOK_URL = "https://hooks.slack.com/services/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"
+WEB_HOOK_URL = "https://hooks.slack.com/services/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"
 
 def shuttaikinNotify(msg):
 	requests.post(WEB_HOOK_URL, data=json.dumps({
