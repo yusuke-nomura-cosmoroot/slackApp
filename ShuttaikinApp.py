d93df5cbed7e6a670c83c679c5c020c68d34d14c
yusuke-nomura-cosmoroot <y_nomura@cosmoroot.co.jp>
2020-10-01 09:04:26 +0900

    URLを阿部さんのものに変更

--- ShuttaikinApp.py
+++ ShuttaikinApp.py
@@ -4,7 +4,7 @@ import sys
 import requests
 import json
 
-WEB_HOOK_URL = "https://hooks.slack.com/services/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"
+WEB_HOOK_URL = "https://hooks.slack.com/services/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"
 
 def shuttaikinNotify(msg):
 	requests.post(WEB_HOOK_URL, data=json.dumps({
